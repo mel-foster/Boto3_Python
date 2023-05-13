706cf9721a8de358906507bf5b0a950d8f0f7ee9
mel.foster <melfreeman83@gmail.com>
2023-05-13 03:05:06 +0000

    Updated per Hands On Demo

--- list_objects.py
+++ list_objects.py
@@ -1,15 +1,46 @@
 import boto3
 
-s3 = boto3.client('s3')
-
-response = s3.list_objects_v2(Bucket="mfoster-boto3-050123")
-
-extension = ".txt"
-
-for content in response["Contents"]:
+def filter_objects_extension(client, bucket, extension):
+    keys = []
+    response = client.list_objects_v2(Bucket=bucket)
+    for content in response["Contents"]:
+        if(extension in content["Key"][-(len(extension)):]):
+            keys.append(content["Key"])
+            
+    return keys
     
-    if(extension in content["Key"][-(len(extension)):]): 
-        print(content["Key"])
+def list_objects_keys(client, bucket, prefix=""):
+    keys = []
+    response = client.list_objects_v2(Bucket=bucket, Prefix=prefix)
+    for content in response["Contents"]:
+        keys.append(content["Key"])
+            
+    return keys
+
+
+if __name__ == '__main__':
+    s3 = boto3.client('s3')
+    
+    response = list_objects_keys(s3, "mfoster-boto3-050123")
+    print(response)
+    
+    
+    response = filter_objects_extension(s3, "mfoster-boto3-050123")
+    print(response)
+
+
+
+
+#s3 = boto3.client('s3')
+
+#response = s3.list_objects_v2(Bucket="mfoster-boto3-050123")
+
+#extension = ".txt"
+
+#for content in response["Contents"]:
+    
+    #if(extension in content["Key"][-(len(extension)):]): 
+    #    print(content["Key"])
 
 
 
