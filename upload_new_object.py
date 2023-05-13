2f5d0c3233da046b2558e36e878f34f5b6c201a8
mel.foster <melfreeman83@gmail.com>
2023-05-13 03:16:39 +0000

    updated to include more information regarding deleting inside folder

--- upload_new_object.py
+++ upload_new_object.py
@@ -5,3 +5,7 @@ s3 = boto3.client('s3')
 
 s3.put_object(Bucket="mfoster-boto3-050123", Key="test_file_string.txt", Body="Hey, this is a string", ContentType="text/plain")
  
+#s3.put_objects(Bucket="mfoster-boto3-050123",
+                #Key="folder/foldera/folder1/test_test_string.txt,
+                #Body="Hey this is a string",
+                #ContentType= "text/plain")
