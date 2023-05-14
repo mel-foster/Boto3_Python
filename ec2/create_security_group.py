2f401e815124357223f4a9492f95fd7d1ca5725e
mel.foster <melfreeman83@gmail.com>
2023-05-14 04:08:50 +0000

    Creating a Security Group for VPC

---
+++ ec2/create_security_group.py
@@ -0,0 +1,11 @@
+import boto3
+
+ec2 = boto3.client('ec2')
+
+response = ec2.create_security_group(
+    Description='SG from boto3',
+    GroupName='boto3-security-group',
+    VpcId='vpc-036c6205cf8d63f01',
+)
+
+print(response["GroupId"])
