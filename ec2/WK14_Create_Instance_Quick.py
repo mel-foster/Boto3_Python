1db4a769cc1aac5a51e21bf52a7374553c907d57
mel.foster <melfreeman83@gmail.com>
2023-05-16 01:58:03 +0000

    Quick Create Instance Script

---
+++ ec2/WK14_Create_Instance_Quick.py
@@ -0,0 +1,11 @@
+import boto3
+
+ec2 = boto3.resource ('ec2')
+
+instance = ec2.create_instances(
+    ImageId = 'ami-0889a44b331db0194',
+    MinCount = 3,
+    MaxCount = 3, 
+    InstanceType = 't2.micro',
+    )
+print(instance)
