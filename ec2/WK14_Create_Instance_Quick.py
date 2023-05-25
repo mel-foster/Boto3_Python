03a82d5294e7ad2ff9f8f1dede262b3fd7b92689
mel.foster <melfreeman83@gmail.com>
2023-05-25 00:43:12 +0000

    Added in pairkey

--- ec2/WK14_Create_Instance_Quick.py
+++ ec2/WK14_Create_Instance_Quick.py
@@ -6,7 +6,16 @@ instance = ec2.create_instances(
     ImageId = 'ami-0889a44b331db0194',
     MinCount = 3,
     MaxCount = 3, 
+   
+#ADDING IN ADDITIONAL INFORMATION & TAGS
     InstanceType = 't2.micro',
-    )
+    KeyName= 'COMPLEXWK14',
+    TagSpecifications=[
+            {
+                'ResourceType': 'instance',
+                'Tags': [{'Key': 'Name','Value': 'Linux Server'},
+                {'Key': 'Env','Value': 'Dev'}]
+                          },
+                      ],
+                      )
 print(instance)
-
