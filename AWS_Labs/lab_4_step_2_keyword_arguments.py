abeb6b22758963767009ee39ebfd25f0171e7228
EC2 Default User <ec2-user@ip-172-31-19-89.ec2.internal>
2023-06-21 18:05:28 +0000

    lab 4

---
+++ AWS_Labs/lab_4_step_2_keyword_arguments.py
@@ -0,0 +1,20 @@
+import boto3
+
+def translate_text(**kwargs): 
+    client = boto3.client('translate')
+    response = client.translate_text(**kwargs)
+    print(response) 
+
+### Change below this line only ###
+
+kwargs={
+    "Text":"I am learning to code in AWS",
+    "SourceLanguageCode":"en",
+    "TargetLanguageCode":"fr"
+    }
+
+def main():
+    translate_text(**kwargs)
+
+if __name__=="__main__":
+    main()
