094ea14655c7ca66a6ca2e4e8b8e2a6e09c47ac5
mel-foster <melfreeman83@gmail.com>
2023-06-21 19:29:05 +0000

    update movie query.py files

---
+++ AWS_Labs/MoviesQuery01.py
@@ -0,0 +1,27 @@
+import boto3
+import json
+import decimal
+from boto3.dynamodb.conditions import Key, Attr
+
+# Helper class to convert a DynamoDB item to JSON.
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if o % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
+
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+
+table = dynamodb.Table('Movies')
+
+print("Movies from 1985")
+
+response = table.query(
+    KeyConditionExpression=Key('year').eq(1985)
+)
+
+for i in response['Items']:
+    print(i['year'], ":", i['title'])
